87ba373ef2c6dbfdcddef1a21e8f76de4967e212
Lucas Sotelo <lucasnsotelog@gmail.com>
2021-12-17 20:50:41 -0300

    Botones de acciones en listado (#25)
    * feat: botones de borrado y edición de proyectos
    
    * feat: handle boton editar
    
    * refactor: extraer alert en un componente aparte
    
    * refactor: extraer botones de acciones
    
    * feat: tooltip a botones de acciones

--- src/components/ListadoProyectos.tsx
+++ src/components/ListadoProyectos.tsx
@@ -7,12 +7,19 @@ import {
   Td,
   Spinner,
   Flex,
+  useToast,
+  useDisclosure,
 } from '@chakra-ui/react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import { proyectosAPI } from '../axios'
 import EmptyProyectos from './EmptyProyectos'
+import AdvertenciaModal from './AdvertenciaModal'
+import ActionButtons from './ActionButtons'
 
 interface Proyecto {
-  id: number
+  _id: number
   nombre: string
   tipo: string
   estado: string
@@ -22,9 +29,38 @@ interface Proyecto {
 interface Props {
   proyectos: Proyecto[]
   loading: boolean
+  setProyectos: any
 }
 
-const ListadoProyectos = ({ proyectos, loading }: Props) => {
+const ListadoProyectos = ({ proyectos, setProyectos, loading }: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const [proyectoABorrar, setProyectoABorrar] = useState<any>()
+  const navigate = useNavigate()
+
+  const handleEdit = (proyectoId: any) => {
+    navigate(`/proyectos/${proyectoId}/editar`)
+  }
+
+  const handleDelete = async (proyectoId: any) => {
+    try {
+      await proyectosAPI.delete(`/projects/${proyectoId}`)
+      toast({
+        title: 'Proyecto borrado 👌',
+        status: 'success',
+        isClosable: true,
+      })
+      const { data } = await proyectosAPI.get('projects')
+      setProyectos(data.message)
+    } catch (err) {
+      toast({
+        title: 'Ocurrió un error al intentar borrar el proyecto 😔',
+        status: 'error',
+        isClosable: true,
+      })
+    }
+  }
+
   if (loading) {
     return (
       <Flex p="5px" w="100%" justifyContent="center" alignItems="center">
@@ -36,28 +72,56 @@ const ListadoProyectos = ({ proyectos, loading }: Props) => {
   return proyectos && proyectos.length === 0 ? (
     <EmptyProyectos />
   ) : (
-    <Table variant="striped" colorScheme="teal">
-      <Thead>
-        <Tr>
-          <Th>Nombre</Th>
-          <Th>Tipo</Th>
-          <Th>Estado</Th>
-          <Th>Líder</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {proyectos.map((proyecto) => {
-          return (
+    <>
+      <Table variant="striped" colorScheme="teal">
+        <Thead>
+          <Tr>
+            <Th>Nombre</Th>
+            <Th>Tipo</Th>
+            <Th>Estado</Th>
+            <Th>Líder</Th>
+            <Th>Acciones</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {proyectos.map((proyecto) => (
             <Tr>
               <Td>{proyecto.nombre}</Td>
               <Td>{proyecto.tipo}</Td>
               <Td>{proyecto.estado}</Td>
               <Td>{proyecto.liderProyecto}</Td>
+              <Td w="100px">
+                <ActionButtons
+                  onDelete={() => {
+                    setProyectoABorrar(proyecto)
+                    onOpen()
+                  }}
+                  onEdit={() => {
+                    handleEdit(proyecto._id)
+                  }}
+                />
+              </Td>
             </Tr>
-          )
-        })}
-      </Tbody>
-    </Table>
+          ))}
+        </Tbody>
+      </Table>
+
+      {proyectoABorrar && (
+        <AdvertenciaModal
+          isOpen={isOpen}
+          onClose={onClose}
+          onDelete={() => handleDelete(proyectoABorrar._id)}
+          proyecto={proyectoABorrar}
+          alertHeader="Borrar Proyecto"
+          alertBody={
+            <>
+              ¿Estás seguro que querés borrar{' '}
+              <strong>{proyectoABorrar.nombre}</strong> de forma permanente?
+            </>
+          }
+        />
+      )}
+    </>
   )
 }
 
