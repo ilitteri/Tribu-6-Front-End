26de92e54d163592a2f11d2178ab023f98abd1bd
Lucas Sotelo <lucasnsotelog@gmail.com>
2021-12-17 00:01:47 -0300

    feat: tooltip a botones de acciones

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -1,24 +1,29 @@
 import { IconButton } from '@chakra-ui/button'
 import { Flex } from '@chakra-ui/layout'
+import { Tooltip } from '@chakra-ui/react'
 import { FaTrashAlt, FaEdit } from 'react-icons/fa'
 
 const ActionButtons = ({ onDelete, onEdit }: any) => {
   return (
     <Flex justifyContent="center" alignItems="center">
-      <IconButton
-        aria-label="Borrar"
-        variant="outline"
-        icon={<FaTrashAlt />}
-        color="red.500"
-        mr="10px"
-        onClick={onDelete}
-      />
-      <IconButton
-        aria-label="Editar"
-        variant="outline"
-        icon={<FaEdit />}
-        onClick={onEdit}
-      />
+      <Tooltip hasArrow label="Borrar">
+        <IconButton
+          aria-label="Borrar"
+          variant="outline"
+          icon={<FaTrashAlt />}
+          color="red.500"
+          mr="10px"
+          onClick={onDelete}
+        />
+      </Tooltip>
+      <Tooltip hasArrow label="Editar">
+        <IconButton
+          aria-label="Editar"
+          variant="outline"
+          icon={<FaEdit />}
+          onClick={onEdit}
+        />
+      </Tooltip>
     </Flex>
   )
 }
