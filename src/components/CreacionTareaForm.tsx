ec5ead8510703c2787c04fd50edffcb0f8c3be1e
Lucas Sotelo <lucasnsotelog@gmail.com>
2021-12-18 17:30:29 -0300

    feat:  use recursos api

--- src/components/CreacionTareaForm.tsx
+++ src/components/CreacionTareaForm.tsx
@@ -18,7 +18,7 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
-import { proyectosAPI } from '../axios'
+import { proyectosAPI, recursosAPI } from '../axios'
 
 interface Proyecto {
   _id: number
@@ -36,24 +36,6 @@ interface Recurso {
   Nombre: string
   Apellido: string
 }
-
-const mockRecursos: Recurso[] = [
-  {
-    legajo: 1,
-    Nombre: 'Mario',
-    Apellido: 'Mendoza',
-  },
-  {
-    legajo: 2,
-    Nombre: 'Maria',
-    Apellido: 'Perez',
-  },
-  {
-    legajo: 3,
-    Nombre: 'Patricia',
-    Apellido: 'Gaona',
-  },
-]
 
 const CreacionTareaForm = () => {
   const [proyectos, setProyectos] = useState<Proyecto[]>([])
@@ -78,10 +60,8 @@ const CreacionTareaForm = () => {
     }
 
     const fetchRecursos = async () => {
-      // TODO: Descomentar al resolver problema de cors
-      // const res = await recursosAPI.get('/recursos')
-      // setRecursos(res.data)
-      setRecursos(mockRecursos)
+      const res = await recursosAPI.get('/recursos')
+      setRecursos(res.data)
     }
 
     fetchProyectos()
