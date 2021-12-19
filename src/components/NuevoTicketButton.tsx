9ee649c2dee1c76398fa75f116069bb5b4df5d34
felicoper <felipec@eudaimonia.com.ar>
2021-12-19 13:00:51 -0300

    refactor get versiones y productos

--- src/components/NuevoTicketButton.tsx
+++ src/components/NuevoTicketButton.tsx
@@ -1,13 +1,22 @@
 import { Button } from '@chakra-ui/react'
 import { GoPlus } from 'react-icons/go'
-import { useNavigate } from 'react-router-dom'
+import { createSearchParams, useNavigate } from 'react-router-dom'
 
-const NuevoTicketButton = () => {
+const NuevoTicketButton = ( params: any ) => {
   const navigate = useNavigate()
+
+  const searchParams = params.version
+  ? `?${createSearchParams({
+    versionId: params.version,
+    })}`
+  : ''
 
   return (
     <Button
-      onClick={() => navigate('/soporte/nuevo')}
+      onClick={() => navigate({
+        pathname: '/soporte/nuevo',
+        search: searchParams,
+      })}
       leftIcon={<GoPlus />}
       colorScheme="teal"
       variant="solid"
