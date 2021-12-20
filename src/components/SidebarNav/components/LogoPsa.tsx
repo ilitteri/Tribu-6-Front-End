ea735ce5ccc9b90036e4edf6b47f6910b4e376cf
Lucas Sotelo <lucasnsotelog@gmail.com>
2021-12-20 00:47:08 -0300

    imagen clickeable (#38)

--- src/components/SidebarNav/components/LogoPsa.tsx
+++ src/components/SidebarNav/components/LogoPsa.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Image } from '@chakra-ui/react'
 import LogoPSANavidad from '../../../assets/logo_navidad.png'
+import { useNavigate } from 'react-router'
 
-const LogoPsa = ({ ...rest }) => (
-  <Image p="4px" maxH="100%" src={LogoPSANavidad} alt="Logo psa" {...rest} />
-)
+const LogoPsa = ({ ...rest }) => {
+  const navigate = useNavigate()
+
+  return (
+    <Image
+      onClick={() => navigate('/')}
+      cursor="pointer"
+      p="4px"
+      maxH="100%"
+      src={LogoPSANavidad}
+      alt="Logo psa"
+      {...rest}
+    />
+  )
+}
 
 export default LogoPsa
